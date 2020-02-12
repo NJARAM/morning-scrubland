067712f674857a976abf1bcddaf6dddf75d8d1e6
Moses Njara <njaramoses@gmail.com>
2020-02-12 10:08:56 +0300

    add datatables

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,8 +21,10 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'DataTables/datatables.min.css',
     ];
     public $js = [
+        'DataTables/datatables.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
