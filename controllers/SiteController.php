05a4c577fb1a3bcf4a2736986791fca4dc7af1dc
Moses Njara <njaramoses@gmail.com>
2020-02-12 22:37:37 +0300

    configuration

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -94,7 +94,7 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        return $this->redirect('/companyproduct/index');
     }
 
     /**
