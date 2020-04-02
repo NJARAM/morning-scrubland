fbac7f5313e6b8eb77bbb9912613f4db0ad5605a
Moses Njara <njaramoses@gmail.com>
2020-04-02 18:04:29 +0300

    Disable Project

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -15,28 +15,28 @@ class SiteController extends Controller
     /**
      * {@inheritdoc}
      */
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['logout'],
-                'rules' => [
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
-    }
+    // public function behaviors()
+    // {
+    //     return [
+    //         'access' => [
+    //             'class' => AccessControl::className(),
+    //             'only' => ['logout'],
+    //             'rules' => [
+    //                 [
+    //                     'actions' => ['logout'],
+    //                     'allow' => true,
+    //                     'roles' => ['@'],
+    //                 ],
+    //             ],
+    //         ],
+    //         'verbs' => [
+    //             'class' => VerbFilter::className(),
+    //             'actions' => [
+    //                 'logout' => ['post'],
+    //             ],
+    //         ],
+    //     ];
+    // }
 
     /**
      * {@inheritdoc}
@@ -57,105 +57,105 @@ class SiteController extends Controller
     /**
      * @return array
      */
-    public function actions()
-    {
-        return [
-            'login' => [
-                'class' => 'yii2mod\user\actions\LoginAction'
-            ],
-            'logout' => [
-                'class' => 'yii2mod\user\actions\LogoutAction'
-            ],
-            'signup' => [
-                'class' => 'yii2mod\user\actions\SignupAction'
-            ],
-            'request-password-reset' => [
-                'class' => 'yii2mod\user\actions\RequestPasswordResetAction'
-            ],
-            'password-reset' => [
-                'class' => 'yii2mod\user\actions\PasswordResetAction'
-            ],
+    // public function actions()
+    // {
+    //     return [
+    //         'login' => [
+    //             'class' => 'yii2mod\user\actions\LoginAction'
+    //         ],
+    //         'logout' => [
+    //             'class' => 'yii2mod\user\actions\LogoutAction'
+    //         ],
+    //         'signup' => [
+    //             'class' => 'yii2mod\user\actions\SignupAction'
+    //         ],
+    //         'request-password-reset' => [
+    //             'class' => 'yii2mod\user\actions\RequestPasswordResetAction'
+    //         ],
+    //         'password-reset' => [
+    //             'class' => 'yii2mod\user\actions\PasswordResetAction'
+    //         ],
 
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
+    //         'error' => [
+    //             'class' => 'yii\web\ErrorAction',
+    //         ],
+    //         'captcha' => [
+    //             'class' => 'yii\captcha\CaptchaAction',
+    //             'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+    //         ],
 
-        ];
-    }
+    //     ];
+    // }
 
     /**
      * Displays homepage.
      *
      * @return string
      */
-    public function actionIndex()
-    {
-        return $this->redirect('/companyproduct/index');
-    }
+    // public function actionIndex()
+    // {
+    //     return $this->redirect('/companyproduct/index');
+    // }
 
     /**
      * Login action.
      *
      * @return Response|string
      */
-    public function actionLogin()
-    {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
+    // public function actionLogin()
+    // {
+    //     if (!Yii::$app->user->isGuest) {
+    //         return $this->goHome();
+    //     }
 
-        $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
-        }
+    //     $model = new LoginForm();
+    //     if ($model->load(Yii::$app->request->post()) && $model->login()) {
+    //         return $this->goBack();
+    //     }
 
-        $model->password = '';
-        return $this->render('login', [
-            'model' => $model,
-        ]);
-    }
+    //     $model->password = '';
+    //     return $this->render('login', [
+    //         'model' => $model,
+    //     ]);
+    // }
 
     /**
      * Logout action.
      *
      * @return Response
      */
-    public function actionLogout()
-    {
-        Yii::$app->user->logout();
+    // public function actionLogout()
+    // {
+    //     Yii::$app->user->logout();
 
-        return $this->goHome();
-    }
+    //     return $this->goHome();
+    // }
 
     /**
      * Displays contact page.
      *
      * @return Response|string
      */
-    public function actionContact()
-    {
-        $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
-            Yii::$app->session->setFlash('contactFormSubmitted');
+    // public function actionContact()
+    // {
+    //     $model = new ContactForm();
+    //     if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
+    //         Yii::$app->session->setFlash('contactFormSubmitted');
 
-            return $this->refresh();
-        }
-        return $this->render('contact', [
-            'model' => $model,
-        ]);
-    }
+    //         return $this->refresh();
+    //     }
+    //     return $this->render('contact', [
+    //         'model' => $model,
+    //     ]);
+    // }
 
     /**
      * Displays about page.
      *
      * @return string
      */
-    public function actionAbout()
-    {
-        return $this->render('about');
-    }
+    // public function actionAbout()
+    // {
+    //     return $this->render('about');
+    // }
 }
